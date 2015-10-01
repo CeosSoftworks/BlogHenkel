46a892e2aee746893265e32aefafc911341d0fac
Jeferson dos Santos Oliveira <jefersong1@yahoo.com.br>
2015-10-01 02:41:32 -0300

    Estilização da barra de menu lateral

--- wp-content/themes/BlogHenkel/sidebar.php
+++ wp-content/themes/BlogHenkel/sidebar.php
@@ -3,26 +3,26 @@
   <div class="inner padding-y-0 padding-x-10px padding-right-0">
     <header id="site-header">
       <h1 id="site-branding">
-        <div id="site-logo-wrap">
+        <div id="site-logo-wrap aligncenter">
           <a href="<?= home_url() ?>">
             <img id="site-logo" src="<?= get_template_directory_uri() ?>/imgs/logo.png">
           </a>
         </div>
       </h1>
-      <div id="site-social-links">
-        <span class="social-link-wrap col-xs-3 no-padding"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-facebook.png"></a></span>
-        <span class="social-link-wrap col-xs-3 no-padding"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-gplus.png"></a></span>
-        <span class="social-link-wrap col-xs-3 no-padding"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-youtube.png"></a></span>
-        <span class="social-link-wrap col-xs-3 no-padding"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-twitter.png"></a></span>
+      <div id="site-social-links" class="aligncenter container-fluid padding-x-0">
+        <span class="social-link-wrap col-xs-3 no-padding"><div class="padding-x-5px"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-facebook.png"></a></div></span>
+        <span class="social-link-wrap col-xs-3 no-padding"><div class="padding-x-5px"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-gplus.png"></a></div></span>
+        <span class="social-link-wrap col-xs-3 no-padding"><div class="padding-x-5px"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-youtube.png"></a></div></span>
+        <span class="social-link-wrap col-xs-3 no-padding"><div class="padding-x-5px"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-twitter.png"></a></div></span>
       </div>
-      <div id="site-langs">
-        <span class="lang-link-wrap col-xs-3 no-padding"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-brazil.png"></a></span>
-        <span class="lang-link-wrap col-xs-3 no-padding"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-argentina.png"></a></span>
-        <span class="lang-link-wrap col-xs-3 no-padding"><a href="#"><img src="<?= get_template_directory_uri() ?>/imgs/icon-chile.png"></a></span>
+      <div id="site-langs" class="aligncenter container-fluid padding-x-0 padding-top-20px">
+        <span class="lang-link-wrap col-xs-3 no-padding"><a href="#"><div class="padding-x-5px"><img src="<?= get_template_directory_uri() ?>/imgs/icon-brazil.png"></a></div></span>
+        <span class="lang-link-wrap col-xs-3 no-padding"><a href="#"><div class="padding-x-5px"><img src="<?= get_template_directory_uri() ?>/imgs/icon-argentina.png"></a></div></span>
+        <span class="lang-link-wrap col-xs-3 no-padding"><a href="#"><div class="padding-x-5px"><img src="<?= get_template_directory_uri() ?>/imgs/icon-chile.png"></a></div></span>
       </div>
       <div id="site-search"><?php get_search_form() ?></div>
     </header>
-    <div id="sidebar-content">
+    <div id="sidebar-content" class="padding-top-20px">
       <?php wp_list_categories(array(
           'title_li' => __('Mais neste blog'),
           'hide_empty' => false,
