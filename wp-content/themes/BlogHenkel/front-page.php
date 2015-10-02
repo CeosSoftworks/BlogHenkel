b15a38197635ee1ce4da3a741c0df9985c66aaae
Jeferson dos Santos Oliveira <jefersong1@yahoo.com.br>
2015-10-02 01:44:46 -0300

    Ajustes na altura dos posts e adição de campo para banner na homepage.

--- wp-content/themes/BlogHenkel/front-page.php
+++ wp-content/themes/BlogHenkel/front-page.php
@@ -16,7 +16,12 @@
     <div class="inner padding-y-0 padding-x-10px"><?php get_template_part('php/boxed-links') ?></div>
   </div>
   <div class="col-md-4">
-    <div class="inner padding-y-0 padding-x-10px">Apoio</div>
+    <div class="inner padding-y-0 padding-x-10px">
+      <div id="banner-wrap"><img src="<?= get_template_directory_uri() ?>/imgs/banner.png" style="width: 100%"></div>
+      <div id="page-support">
+        <img src="<?= get_template_directory_uri() ?>/imgs/clipart-support-henkel.png">
+      </div>
+    </div>
   </div>
 </section>
 
