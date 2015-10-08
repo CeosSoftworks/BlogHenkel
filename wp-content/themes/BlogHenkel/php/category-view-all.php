4620fbefdf06a957f2c87604deb608c7324be115
Jeferson dos Santos Oliveira <jefersong1@yahoo.com.br>
2015-10-08 05:56:45 -0300

    Melhorias na estilização da seção "Ver todos os posts"

--- wp-content/themes/BlogHenkel/php/category-view-all.php
+++ wp-content/themes/BlogHenkel/php/category-view-all.php
@@ -9,13 +9,29 @@ $cvaQuery = new WP_Query(array(
   'posts_per_page' => 6
 ));
 
-echo "<h4><a href=\"\">" . __('Todos os posts deste canal') . ":</a></h4>";
-
-if($cvaQuery->have_posts()) {
-  while($cvaQuery->have_posts()) {
-    $cvaQuery->the_post();
-    get_template_part('php/post', 'mini');
-  }
-}
-
-wp_reset_postdata();
+?>
+<aside id="cat-view-all">
+  <h6><a href="#"><?= __('Todos os posts deste canal') ?>:</a></h6>
+  <div class="inner clearfix">
+    <?php if($cvaQuery->have_posts()) : ?>
+      <?php while($cvaQuery->have_posts()) : $cvaQuery->the_post() ?>
+        <div class="post-mini-wrap col-sm-6">
+          <a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
+            <article id="post-<?php the_ID() ?>" <?php post_class('row vertical-middle') ?>>
+              <div class="post-content-wrap post-header-img col-xs-5" style="background-image: url(<?= getPostFeaturedImageUrl(get_the_ID()) ?>)"></div>
+              <div class="post-content-wrap col-xs-7">
+                <header class="post-header">
+                  <div class="inner">
+                    <h4 class="post-cat"><?= $categories[0]->name ?></h4>
+                    <h1 class="post-title"><?php the_title() ?></h1>
+                  </div>
+                </header>
+              </div>
+            </article>
+          </a>
+        </div>
+      <?php endwhile ?>
+    <?php endif ?>
+    <?php wp_reset_postdata() ?>
+  </div>
+</aside>
